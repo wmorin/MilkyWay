56cfcf6cc3b34fddf9ab23092efc299a6c825649
Willy Morin <willy.morin@blablacar.com>
2015-03-09 21:49:00 +0100

    Simplify flow Interface

--- src/Milkyway/Flow/FlowInterface.php
+++ src/Milkyway/Flow/FlowInterface.php
@@ -4,10 +4,8 @@ namespace Milkyway\Flow;
 
 interface FlowInterface
 {
-    public function setExpiresAt($time);
-    public function getExpiresAt();
-    public function setOpacity($opacity);
-    public function getOpacity();
-    public function setTitle($title);
-    public function getTitle();
+    /**
+     * @return array
+     */
+    public function toArray();
 }
